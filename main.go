c2ca5341236dd17817a71f1a56d5b6a58270f0ad
Pavel Larin <larinpavelfirst@mail.ru>
2023-10-30 13:30:32 -0300

    Go slices, append, 2d slices, range

--- main.go
+++ main.go
@@ -349,36 +349,343 @@
 
 // Note
 
-// Make note of how the variadic inputs and the spread operator are
-// used in the test suite.
+// // Make note of how the variadic inputs and the spread operator are
+// // used in the test suite.
 
+// package main
+
+// import "fmt"
+
+// func sum(nums ...float64) float64 {
+// 	sum := 0.0
+// 	for i := 0; i < len(nums); i++ {
+// 		num := nums[i]
+// 		sum += num
+// 	}
+// 	return sum
+// }
+
+// // don't edit below this line
+
+// func test(nums ...float64) {
+// 	total := sum(nums...)
+// 	fmt.Printf("Summing %v costs...\n", len(nums))
+// 	fmt.Printf("Bill for the month: %.2f\n", total)
+// 	fmt.Println("===== END REPORT =====")
+// }
+
+// func main() {
+// 	test(1.0, 2.0, 3.0)
+// 	test(1.0, 2.0, 3.0, 4.0, 5.0)
+// 	test(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0)
+// 	test(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0)
+// }
+
+//// 2023/10/30 // 20:25 //
+//// Append
+
+// // The built-in append function is used to dynamically add elements to
+// // a slice:
+// func append(slice []Type, elems ...Type) []Type
+
+// // If the underlying array is not large enough, append() will create a
+// // new underlying array and poitn the slice to it.
+
+// // Notice that append() is variadic, the following are valid:
+// slice = append(slice, oneThing)
+// slice = append(slice, firstThing, secondThing)
+// slice = append(slice, anotherSlice...)
+
+// // Assignment
+
+// // We've been asked to "bucket" costs for an entire month into the
+// // cost occurring on each day of the month.
+
+// // Complete the getCostsByDay function. It should return a slice of
+// // float64s, where each element is the total cost for that day. The
+// // length of the slice should be equal to the number of days
+// // represented in the costs slice, including any days that have no
+// // costs, up to the last day represented in the slice.
+
+// // Days are numbered and start at 0.
+
+// // Example
+
+// // Input in day, cost format:
+// []cost{
+// 	{0, 4.0},
+// 	{1, 2.1},
+// 	{1, 3.1},
+// 	{5, 2.5},
+// }
+
+// // // It would expect this resutl:
+// // []float64{
+// // 	4.0,
+// // 	5.2,
+// // 	0.0,
+// // 	0.0,
+// // 	0.0,
+// // 	2.5,
+// // }
+
+// package main
+
+// import "fmt"
+
+// type cost struct {
+// 	day   int
+// 	value float64
+// }
+
+// func getCostsByDay(costs []cost) []float64 {
+// 	costsByDay := []float64{}
+// 	for i := 0; i < len(costs); i++ {
+// 		cost := costs[i]
+// 		for cost.day >= len(costsByDay) {
+// 			costsByDay = append(costsByDay, 0.0)
+// 		}
+// 		costsByDay[cost.day] += cost.value
+// 	}
+// 	return costsByDay
+// }
+
+// func test(costs []cost) {
+// 	fmt.Printf("Creating daily buckets for %v costs...\n", len(costs))
+// 	costsByDay := getCostsByDay(costs)
+// 	fmt.Println("Costs by day:")
+// 	for i := 0; i < len(costsByDay); i++ {
+// 		fmt.Printf(" - Day %v: %.2f\n", i, costsByDay[i])
+// 	}
+// 	fmt.Println("===== END REPORT =====")
+// }
+
+// func main() {
+// 	test([]cost{
+// 		{0, 1.0},
+// 		{1, 2.0},
+// 		{1, 3.1},
+// 		{2, 2.5},
+// 		{3, 3.6},
+// 		{3, 2.7},
+// 		{4, 3.34},
+// 	})
+// 	test([]cost{
+// 		{0, 1.0},
+// 		{10, 2.0},
+// 		{3, 3.1},
+// 		{2, 2.5},
+// 		{1, 3.6},
+// 		{2, 2.7},
+// 		{4, 56.34},
+// 		{13, 2.34},
+// 		{28, 1.34},
+// 		{25, 2.34},
+// 		{30, 4.34},
+// 	})
+// }
+
+//// 20:39
+//// Slice of slices
+
+// // Slices can hold otehr slices, effectively creating a matrix, or 2D slice.
+// rows := [][]int{}
+
+// Assignment
+
+// We support various graphs and dashboards on Textio that display
+// message analytics to our users. The UI for our graphs and charts is
+// built on top of a grid system. Let's build some grid logic.
+
+// Complete the createMatrix function. It takes a number of rows and
+// columns and returns a 2D slice of integers where the value of each
+// cell is i * j where i and j are the indexes of the row and
+// column respectively.
+
+// // For example, a 10x10 matrix would look like this:
+// // [0 0 0 0 0 0 0 0 0 0]
+// // [0 1 2 3 4 5 6 7 8 9]
+// // [0 2 4 6 8 10 12 14 16 18]
+// // [0 3 6 9 12 15 18 21 24 27]
+// // [0 4 8 12 16 20 24 28 32 36]
+// // [0 5 10 15 20 25 30 35 40 45]
+// // [0 6 12 18 24 30 36 42 48 54]
+// // [0 7 14 21 28 35 42 49 56 63]
+// // [0 8 16 24 32 40 48 56 64 72]
+// // [0 9 18 27 36 45 54 63 72 81]
+
+// package main
+
+// import "fmt"
+
+// func createMatrix(rows, cols int) [][]int {
+// 	matrix := [][]int{}
+// 	for i := 0; i < rows; i++ {
+// 		row := []int{}
+// 		for j := 0; j < cols; j++ {
+// 			row = append(row, i*j)
+// 		}
+// 		matrix = append(matrix, row)
+// 	}
+// 	return matrix
+// }
+
+// func test(rows, cols int) {
+// 	fmt.Printf("Creating %v x %v matrix...\n", rows, cols)
+// 	matrix := createMatrix(rows, cols)
+// 	for i := 0; i < len(matrix); i++ {
+// 		fmt.Println(matrix[i])
+// 	}
+// 	fmt.Println("===== END REPORT =====")
+// }
+
+// func main() {
+// 	test(3, 3)
+// 	test(5, 5)
+// 	test(10, 10)
+// 	test(15, 15)
+// }
+
+//// 20:54
+//// Tricky Slices
+
+// // The append() function changes the underlying array of its
+// // parameter AND returns a new slice. This means that using append()
+// // on anything other than itself is usually a BAD idea.
+
+// // dont do this!
+// someSlice = append(otherSlice, element)
+
+// // Take a look at these head-scratchers:
+
+// // Example 1: Works as expected
+// a := make([]int, 3)
+// fmt.Println("len of a:", len(a))
+// // len of a: 3
+// fmt.Println("cap of a:", cap(a))
+// // cap of a: 3
+// fmt.Println("appending 4 to b from a")
+// // appending 4 to b from a
+// b := append(a, 4)
+// fmt.Println("b:", b)
+// // b: [0 0 0 4]
+// fmt.Println("addr of b:", &b[0])
+// // addr of b: 0x44a0c0
+// fmt.Println("appending 5 to c from a")
+// // appending 5 to c from a
+// c := append(a, 5)
+// fmt.Println("addr of c:", &c[0])
+// // addr of c: 0x44a180
+// fmt.Println("a:", a)
+// // a: [0 0 0]
+// fmt.Println("b:", b)
+// // b: [0 0 0 4]
+// fmt.Println("c:", c)
+// // c: [0 0 0 5]
+
+// // With slices a, b, and c, 4 and 5 seem to be appended as we
+// // would expect. We can even check the memory addresses and
+// // confirm that b and c point to different underlying arrays.
+
+// // Example 2: Something fishy
+// i := make([]int, 3, 8)
+// fmt.Println("len of i:", len(i))
+// // len of i: 3
+// fmt.Println("cap of i:", cap(i))
+// // cap of i: 8
+// fmt.Println("appending 4 to j from i")
+// // appending 4 to j from i
+// j := append(i, 4)
+// fmt.Println("j:", j)
+// // j: [0 0 0 4]
+// fmt.Println("addr of j:", &j[0])
+// // addr of j: 0x454000
+// fmt.Println("appending 5 to g from i")
+// // appending 5 to g from i
+// g := append(i, 5)
+// fmt.Println("addr of g:", &g[0])
+// // addr of g: 0x454000
+// fmt.Println("i:", i)
+// // i: [0 0 0]
+// fmt.Println("j:", j)
+// // j: [0 0 0 5]
+// fmt.Println("g:", g)
+// // g: [0 0 0 5]
+
+// // In this example howeer, when 5 is appended to g it overwrites
+// // j's fourth index because j and g point to the same underlying array.
+// // The append() function only creates a new array when there
+// // isn't any capacity left. We created i with a length of 3 and a
+// // capacity of 8, which means we can append 5 items before a new
+// // array is automatically allocated.
+
+// // Again, to avoid bugs like this, you should always use the append
+// // function on the same slice the result is assigned to:
+// mySlice := []int{1, 2, 3}
+// mySlcie = append(mySlice, 4)
+
+//// 13:09
+//// Range
+
+// Range
+
+// // Go provides syntactic sugar to iterate easily over elements of a slice:
+// for INDEX, ELEMENT := range SLICE {
+// }
+
+// // For example:
+// fruits := []string{"apple", "banana", "grape"}
+// for i, fruit := range fruits {
+// 	fmt.Println(i, fruit)
+// }
+// // 0 apple
+// // 1 banana
+// // 2 grape
+
+// Assignment
+
+// We need to be able to quickly detect bad words in the messages our
+// system sends.
+
+// Complete the indexOfFirstBadWord function. If it finds any bad
+// words in the message it should return the index of the first bad
+// word in the msg slice. This will help us filter out naighty words from
+// out messaging system. If no bad words are found, return -1 instead
+
+// Use the range keyword.
 package main
 
 import "fmt"
 
-func sum(nums ...float64) float64 {
-	sum := 0.0
-	for i := 0; i < len(nums); i++ {
-		num := nums[i]
-		sum += num
+func indexOfFirstBadWord(msg []string, badWords []string) int {
+	for i, word := range msg {
+		for _, badWord := range badWords {
+			if word == badWord {
+				return i
+			}
+		}
 	}
-	return sum
+	return -1
 }
 
-// don't edit below this line
-
-func test(nums ...float64) {
-	total := sum(nums...)
-	fmt.Printf("Summing %v costs...\n", len(nums))
-	fmt.Printf("Bill for the month: %.2f\n", total)
-	fmt.Println("===== END REPORT =====")
+func test(msg []string, badWords []string) {
+	i := indexOfFirstBadWord(msg, badWords)
+	fmt.Printf("Scanning message: %v for bad words:\n", msg)
+	for _, badWord := range badWords {
+		fmt.Println(` -`, badWord)
+	}
+	fmt.Printf("Index: %v\n", i)
+	fmt.Println("===================================")
 }
 
 func main() {
-	test(1.0, 2.0, 3.0)
-	test(1.0, 2.0, 3.0, 4.0, 5.0)
-	test(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0)
-	test(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0)
-}
+	badWords := []string{"crap", "shoot", "dang", "frick"}
+	message := []string{"hey", "there", "john"}
+	test(message, badWords)
 
-////
+	message = []string{"ugh", "oh", "my", "frick"}
+	test(message, badWords)
+
+	message = []string{"what", "the", "shoot", "I", "hate", "that", "crap"}
+	test(message, badWords)
+}
